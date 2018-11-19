11dd6206be0faac5163c303e8f178f6730b8c1d7
alecmakeus <alecmarcus.n@gmail.com>
2018-11-19 01:07:01 -0500

    Replace favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,14 +9,14 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Thank you for stopping by.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png' },
       {
         rel: 'stylesheet',
         href: process.env.FONT_URL
