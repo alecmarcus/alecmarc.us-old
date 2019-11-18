4e81a0629d9e043c75682a4b397f1a8fa4dd2262
Alec Marcus <alec@alecmarc.us>
2019-11-18 02:09:00 -0500

    add next/prev project getters to store

--- store/projectView.js
+++ store/projectView.js
@@ -1,20 +1,40 @@
 import projects from '~/assets/projects.json'
 
 export const state = () => ({
-  projectArray: Object.keys(projects),
+  activeProject: null,
   activeProjectId: null,
-  activeProject: null
+  activeProjectIndex: null,
+  projectArray: Object.keys(projects)
 })
 
 export const mutations = {
   setActiveProject(state, projectId) {
     if (projectId != null) {
-      state.activeProjectId = projectId
       state.activeProject = projects[projectId]
+      state.activeProjectId = projectId
+      state.activeProjectIndex = state.projectArray.indexOf(projectId)
     } else {
-      state.activeProjectId = null
       state.activeProject = null
+      state.activeProjectId = null
+      state.activeProjectIndex = null
     }
+  }
+}
+
+export const getters = {
+  nextProjectId: state => {
+    const index =
+      state.activeProjectIndex + 1 < state.projectArray.length
+        ? state.activeProjectIndex + 1
+        : 0
+    return state.projectArray[index]
+  },
+  prevProjectId: state => {
+    const index =
+      state.activeProjectIndex - 1 >= 0
+        ? state.activeProjectIndex - 1
+        : state.projectArray.length - 1
+    return state.projectArray[index]
   }
 }
 
