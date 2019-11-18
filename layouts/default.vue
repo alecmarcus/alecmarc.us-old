41d7ebb49fe6e240683106ded683ad15ba4995bc
Alec Marcus <alec@alecmarc.us>
2019-11-18 02:08:40 -0500

    add hide and scroll show to project page nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,62 @@
 <template>
   <main>
-    <v-primary-nav :class="{projects: this.$route.path === '/projects'}"/>
+    <v-primary-nav
+      ref="nav"
+      :class="{
+        projects: this.$route.path === '/projects',
+        'project-view': activeProject
+      }"
+    />
     <nuxt/>
   </main>
 </template>
 
 <script>
 import VPrimaryNav from '~/components/VPrimaryNav.vue'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     VPrimaryNav
+  },
+  computed: {
+    ...mapState({
+      activeProject: state => state.projectView.activeProject
+    })
+  },
+  watch: {
+    activeProject(val, oldVal) {
+      if (val && !oldVal) {
+        this.handleScroll()
+        window.addEventListener('scroll', this.handleScroll)
+      } else if (!val) {
+        if (this.$refs.nav) {
+          this.$refs.nav.$el.classList.remove('scrolling')
+          this.$refs.nav.$el.style.bottom = 0
+        }
+        window.removeEventListener('scroll', this.handleScroll)
+      }
+    }
+  },
+  mounted() {
+    this.activeProject && this.handleScroll()
+  },
+  destroyed() {
+    if (this.activeProject) {
+      this.$refs.nav.$el.classList.remove('scrolling')
+      window.removeEventListener('scroll', this.handleScroll)
+    }
+  },
+  methods: {
+    handleScroll() {
+      const offset = Math.min(-1 * (150 - window.scrollY), 0)
+      if (this.$refs.nav) {
+        this.$refs.nav.$el.classList.add('scrolling')
+        window.requestAnimationFrame(() => {
+          this.$refs.nav.$el.style.bottom = `${offset}px`
+        })
+      }
+    }
   }
 }
 </script>
@@ -19,7 +65,8 @@ export default {
 main {
   --font-size: #{ms(5)};
   --leading: #{ms($steps: 1, $base: 1)};
-  --nav-height: calc((var(--font-size) * var(--leading)) + (2 * 1.25rem) + 4px);
+  --nav-height: calc((var(--font-size) * var(--leading)) + (2 * 1.25rem));
+  --nav-offset-height: calc(var(--nav-height) + 4px);
 
   line-height: var(--leading);
   font-size: var(--font-size);
@@ -51,9 +98,16 @@ main {
   bottom: 0;
   background: #ffffff;
   z-index: 1;
-}
+  transition-property: min-height, bottom;
+  transition-duration: 0.5s;
+  transition-timing-function: $ease-in-out-circ;
 
-#primaryNav.projects {
-  min-height: 100vh;
+  &.projects {
+    min-height: 100vh;
+  }
+
+  &.scrolling {
+    transition-property: min-height;
+  }
 }
 </style>
