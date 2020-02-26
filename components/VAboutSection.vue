642dfedcad1d58a94c4d52aab59b1ab59d8d6d69
Alec Marcus <alec@alecmarc.us>
2020-02-25 22:36:01 -0500

    udpate projects, add "see also" section

--- components/VAboutSection.vue
+++ components/VAboutSection.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <p>
-      Alec Marcus is a <v-about-snippet project="groove">communication designer</v-about-snippet>, <v-about-snippet project="cor2e-symposium">graphic designer</v-about-snippet>, <v-about-snippet project="lincus">web designer / developer</v-about-snippet>, and maker interested in <v-about-snippet project="hyperreal">media studies</v-about-snippet>, ethical [digital] product design, new & experimental technologies, and <v-about-snippet project="ose">an open source future</v-about-snippet>.
+      Alec Marcus is a <v-about-snippet project="groove">communication designer</v-about-snippet>, <v-about-snippet project="cor2e-facilities">graphic designer</v-about-snippet>, <v-about-snippet project="lincus">entrepeneur, product designer</v-about-snippet>, <a
+        href="https://upstatement.com"
+        target="_blank">web designer, and developer</a> interested in <v-about-snippet project="hyperreal">media studies</v-about-snippet>, ethical [digital] product design, new & experimental technologies, and <v-about-snippet project="ose">an open source future</v-about-snippet>.
     </p>
     <p>
-      He strives to always be learning, subvert questionable relationships with technology, <v-about-snippet project="lincus">promote intellectual pursuit</v-about-snippet>, and modularize everything.
+      He strives to <a
+        href="https://library.upstatement.com"
+        target="_blank">always be learning</a>, subvert questionable relationships with technology, <v-about-snippet project="lincus">promote intellectual pursuit</v-about-snippet>, and modularize everything.
     </p>
     <p>
       He can be reached by <a href="tel:+18603083030">phone</a> or <a href="mailto:hi@alecmarc.us">electronic mail</a>.
     </p>
     <p>
-      He designed and developed this website, which is open source. As such, he invites fellow creatives to <a
+      He designed and developed this website, which is open source. As such, he invites anyone to <a
         href="https://github.com/alecmarcus/alecmarc.us"
-        target="_blank">fork it on GitHub</a>, make it their own, and use it to display their work.
+        target="_blank">fork it on GitHub</a>, improve upon it, make it their own, and use it to display their work.
     </p>
     <p>
       He thanks <a
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,6 +8,29 @@
         :project-year="project.time"
         :project-id="project.id" />
     </ul>
+    <h2>See Also:</h2>
+    <p>
+      <a
+        href="https://hashed.com"
+        target="_blank"><span class="inline-name"><img :src="hashed">Hashed:</span> A South Korean crypto&shy;currency VC empowering companies to rethink money. <img
+          :src="externalLinkIcon"
+          class="ext"></a>
+      <a
+        href="https://typelist.upstatement.com"
+        target="_blank"><span class="inline-name"><img :src="typelist">Typelist:</span> Explore, discover, and get inspired by curated typeface collections. <img
+          :src="externalLinkIcon"
+          class="ext"></a>
+      <a
+        href="https://library.upstatement.com"
+        target="_blank"><span class="inline-name"><img :src="upl">Upstatement Public Library:</span> A catalog of lessons and references on learning to write production code for the web. <img
+          :src="externalLinkIcon"
+          class="ext"></a>
+      <a
+        href="https://cfs.energy"
+        target="_blank"><span class="inline-name"><img :src="cfs">Commonwealth Fusion Systems:</span> An energy startup from MIT harnessing the power of the stars. <img
+          :src="externalLinkIcon"
+          class="ext"></a>
+    </p>
   </section>
 </template>
 
@@ -15,6 +38,11 @@
 import VProjectListItem from '~/components/VProjectListItem.vue'
 import { mapState } from 'vuex'
 import projects from '~/assets/projects.json'
+import externalLinkIcon from '~/assets/icons/external-link-icon.svg'
+import hashed from '~/assets/img/hashed.png'
+import typelist from '~/assets/img/typelist.svg'
+import upl from '~/assets/img/upl.svg'
+import cfs from '~/assets/img/cfs.svg'
 
 export default {
   transition: {
@@ -25,7 +53,12 @@ export default {
     VProjectListItem
   },
   data: () => ({
-    projects
+    projects,
+    externalLinkIcon,
+    hashed,
+    typelist,
+    upl,
+    cfs
   }),
   computed: mapState({
     activeProject: state => state.projectView.activeProject
@@ -35,16 +68,66 @@ export default {
 
 <style lang="scss" scoped>
 section {
-  z-index: 2;
+  margin: var(--nav-offset-height) 0 0;
+  max-height: calc(100vh - (var(--nav-offset-height) + 1.25rem));
+  overflow-y: scroll;
+  padding: 0 ms(1);
   position: relative;
+  z-index: 2;
 }
 
 ul {
-  margin: var(--nav-offset-height) 0 0 1.25rem;
-  padding-top: 1.25rem;
+  padding-bottom: ms(7);
   padding-right: 1.25rem;
-  overflow-y: scroll;
-  max-height: calc(100vh - (var(--nav-offset-height) + 1.25rem));
+  padding-top: 1.25rem;
   position: relative;
+}
+
+h2,
+p {
+  margin: 0;
+  font-size: ms(1);
+
+  @media (min-width: #{$bp-md}) {
+    font-size: ms(2);
+  }
+}
+
+h2 {
+  float: left;
+  margin-bottom: 1em;
+  margin-right: ms(2, 1em);
+}
+
+p {
+  max-width: 27em;
+}
+
+a {
+  hyphens: auto;
+
+  @media (min-width: #{$bp-md-sm}) {
+    hyphens: manual;
+  }
+
+  &:hover,
+  &:focus {
+    img {
+      opacity: 1;
+    }
+  }
+
+  img {
+    transition: opacity $transition-default;
+    display: inline-block;
+    height: ms(-1, 1em);
+    opacity: 0.4;
+    padding-right: ms(-6, 1em);
+    transform: translateY(1px);
+
+    &.ext {
+      padding-right: ms(-2, 1em);
+    }
+  }
 }
 </style>
